04fd8b39381d0b0b7b916e59c745181a98e7cf93
iPaE <pae.papinwit@gmail.com>
2026-04-04 21:03:01 +0700

    move trustbar to StatBarSection Component

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import HeroBanner from "@/components/home/HeroBanner";
+import StatBarSection from "@/components/home/StatBarSection";
 import SectionHeader from "@/components/ui/SectionHeader";
 
 export default function HomePage() {
@@ -8,29 +9,7 @@ export default function HomePage() {
       <HeroBanner />
 
       {/* TRUST BAR */}
-      <section className="bg-navy text-center">
-        <div className="max-w-6xl mx-auto grid grid-cols-2 md:grid-cols-4">
-          {[
-            { num: "10+", label: "Years Experience", th: "ประสบการณ์" },
-            { num: "5K+", label: "Happy Clients", th: "ลูกค้า" },
-            { num: "20+", label: "Services", th: "บริการ" },
-            { num: "100%", label: "Satisfaction", th: "ความพึงพอใจ" },
-          ].map((item, i) => (
-            <div
-              key={i}
-              className="py-12 border-r border-gold/10 last:border-none"
-            >
-              <h3 className="text-4xl font-heading text-gold">{item.num}</h3>
-
-              <p className="text-xs tracking-widest text-gray-400 mt-2 uppercase">
-                {item.label}
-              </p>
-
-              <p className="text-xs text-gray-500 font-thai">{item.th}</p>
-            </div>
-          ))}
-        </div>
-      </section>
+      <StatBarSection />
 
       {/* SERVICES */}
       <section className="py-32 px-6 bg-cream">
