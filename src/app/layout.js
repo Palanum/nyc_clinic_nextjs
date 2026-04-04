ff648f0d2d1d418a62bb2dcff5c6fd8ca42cc3ab
iPaE <pae.papinwit@gmail.com>
2026-04-04 20:25:20 +0700

    move style global.css to /app

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,9 @@
-import "../styles/globals.css";
-
+import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body >
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
